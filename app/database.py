aa4b88bd2999d019efdb13b9935e9581cdfb9be5
Gonzalo2504 <gonzagomezbull@gmail.com>
2024-11-20 20:00:50 -0300

    update database.py

--- app/database.py
+++ app/database.py
@@ -13,16 +13,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Declarative Base
 Base = declarative_base()
 
-# Función para obtener la sesión de la base de datos
-# Esta función utiliza un yield para que se pueda utilizar como un contexto
-# en un with, y se encargue de cerrar la sesión al finalizar
 def get_db():
     db = SessionLocal()
     try:
-        # El yield devuelve el objeto db, que es la sesión de la base de datos
-        # y se puede utilizar dentro del contexto
         yield db
     finally:
-        # Al finalizar el contexto, se cierra la sesión
         db.close()
 
