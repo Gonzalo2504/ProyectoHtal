3a74b147dd10b76a136dff6ed45ea27f86049abd
Gonzalo2504 <gonzagomezbull@gmail.com>
2024-11-24 02:22:30 -0300

    update database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,10 @@
-# Importar las librerías necesarias para la base de datos
-from sqlalchemy import create_engine  # Crear el motor (engine)
-from sqlalchemy.ext.declarative import declarative_base  # Crear la Declarative Base
-from sqlalchemy.orm import sessionmaker  # Crear la sesión de la base de datos
-from .config import DATABASE_URL  # Url de la base de datos
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import DATABASE_URL
 
-# Crear el motor (engine)
 engine = create_engine(DATABASE_URL)
-
-# Crear la sesión de la base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Declarative Base
 Base = declarative_base()
 
 def get_db():
